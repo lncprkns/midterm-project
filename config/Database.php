437e886c33873ae7c0323869a866d0e47f59e4fb
Lance <lance@lncprkns.com>
2022-03-23 17:57:17 -0400

    Updated database config with JawsDB connection info

--- config/Database.php
+++ config/Database.php
@@ -1,20 +1,26 @@
 <?php
     class Database {
 
-        private $host = "localhost";
-        private $db_name = "quotesdb";
-        private $username = "root";
-        private $password = "";
         private $conn;
+        private $url;
 
         // Database connect
-        public function connect() {
+
+        function __construct(){
+            $this->url = getenv('JAWSDB_URL');
             $this->conn = null;
+        }
+
+        public function connect() {
+            $dbparts = parse_url($this->url);
+    
+            $hostname = $dbparts['host'];
+            $username = $dbparts['user'];
+            $password = $dbparts['pass'];
+            $database = ltrim($dbparts['path'],'/');
 
             try {
-                // THIS TRY SECTION NEEDS TO BE UPDATED AFTER UPDATING THE DATABASE PARAMETERS ABOVE
-                $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, 
-                $this->username, $this->password);
+                $this->conn = new PDO("mysql:host=$hostname;dbname=$database", $username, $password);
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch(PDOException $e) {
                 echo "Connection Error: " . $e->getMessage();
